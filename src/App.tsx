46b067e9eaa253c1de3293de6b2244953649342b
Jamaldeen <jd33n27@gmail.com>
2025-12-11 16:46:27 +0100

    trying to style shdcn components without tailwind css

--- src/App.tsx
+++ src/App.tsx
@@ -8,7 +8,7 @@ const App = () => {
     <>
       <AppSidebar className="glass" />
       <div className="min-h-screen w-full bg-sky-500 grid place-items-center relative">
-        <div className="fixed top-0 flex w-full py-2 items-center justify-center glass">
+        <div className="fixed top-3 flex w-full max-w-[1500px] mx-4 py-2 items-center justify-center glass">
           <Navbar className="px-4 py-2 rounded" />
         </div>
         <CardExample />
